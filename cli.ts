f1a7935728d416821144416571fce8cd5112dc68
Joe Roddy <mrjoeroddy@gmail.com>
2023-06-11 20:02:03 -0400

    turn into general purpose cli

---
+++ cli.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+import generate from './generate';
+
+program.version('1.0.0');
+
+program
+  .command('generate')
+  .description('Generates Prisma clients for every schema in the project')
+  .action(() => {
+    console.log('hello world');
+
+    console.log('Generating Prisma clients...');
+
+    generate();
+  });
+
+program.parse(process.argv);
